0ad2839e399c3d31338ac6b0bef3b72c41b24386
kathyavini <kathyavini@gmail.com>
2023-01-23 17:31:05 -0800

    Add put route for likes (13.6) and unified error handling (13.7)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,27 +2,55 @@ const router = require('express').Router();
 
 const { Blog } = require('../models');
 
+const blogFinder = async (req, res, next) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id);
+    if (blog === null) {
+      const error = new Error(`Resource not found: ${req.originalUrl}`);
+      error.statusCode = 404;
+      return next(error);
+    }
+    req.blog = blog;
+    next();
+  } catch (err) {
+    err.statusCode = 400;
+    next(err);
+  }
+};
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const blog = await Blog.create(req.body);
     res.json(blog);
   } catch (err) {
-    res.status(400).json({ err });
+    err.statusCode = 400;
+    next(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.put('/:id', blogFinder, async (req, res, next) => {
   try {
-    const blog = await Blog.findByPk(req.params.id);
-    await blog.destroy();
-    res.json({ deleted: blog });
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
   } catch (err) {
-    res.status(404).json({ error: 'Blog not found' });
+    err.statusCode = 400;
+    next(err);
+  }
+});
+
+router.delete('/:id', blogFinder, async (req, res, next) => {
+  try {
+    await req.blog.destroy();
+    res.json({ deleted: req.blog });
+  } catch (err) {
+    err.statusCode = 400;
+    next(err);
   }
 });
 
--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 
+// Unified error handling
+app.use((err, req, res, next) => {
+  if (!err.statusCode) {
+    err.statusCode = 500;
+  }
+  console.error(err.statusCode, err.message);
+  res.status(err.statusCode).json(err.message);
+});
+
 const start = async () => {
   await connectToDatabase();
   app.listen(PORT, () => {
