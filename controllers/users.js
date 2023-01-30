d910d0cb5ad3146e04a06792c76da04c74a63570
kathyavini <kathyavini@gmail.com>
2023-01-29 19:07:47 -0800

    Add support for users with model and routes for create, get, and update (13.8)

---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+
+const { Blog, User } = require('../models');
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll({
+    //   include: {
+    //     model: Blog,
+    //     attributes: { exclude: ['userId'] },
+    //   },
+    logging: console.log,
+  });
+  res.json(users);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.put('/:username', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.params.username },
+    });
+    user.username = req.body.username;
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    err.statusCode = 400;
+    next(err);
+  }
+});
+
+module.exports = router;
